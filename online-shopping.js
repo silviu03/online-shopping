ebd20799f7c8fe35bfecfecd09485358c81276ab
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-08-22 17:12:25 +0300

    new

--- online-shopping.js
+++ online-shopping.js
@@ -6,8 +6,8 @@ $(function () {
 
     getPropertyHTML = function (productsObj) {
         console.log(productsObj);
-        return `<div data-id=${productsObj.id}>
-                    <div class="product-img" style="background-image: url('poze/${productsObj.imgUrl}')"></div>
+        return `<div class="product-wrapper" data-id=${productsObj.id}>
+                    <div class="product-img" data-id=${productsObj.id} style="background-image: url('poze/${productsObj.imgUrl}')"></div>
                     <div>
                         <span>${productsObj.name}</span>
                         <span>${productsObj.currency} ${productsObj.price}</span>
@@ -26,9 +26,57 @@ $(function () {
         
     }
 
+    const productImg = $('.product-img');
+    const modals = $('#modals');
 
+    productImg.on('click', function (event) {
+        console.log('first')
+        let dataId = event.target.dataset.id -1;
+        console.log(dataId)
+        let overlay = `<div class="overlay" data-id=${products['coats'][dataId].id}>
+        <div class="close-overlay"><i class="fas fa-times"></i></div>
+        <div class="overlay-product-wrapper">
+            <div style="background-image: url('poze/${products['coats'][dataId].imgUrl}')"></div>
+            <div>
+                <h1>${products['coats'][dataId].name}</h1>
+                <span>${products['coats'][dataId].currency} ${products['coats'][dataId].price}</span>
+                <span>Your Size</span>
+                <div class="product-size">
+                    <div><span>S</span></div>
+                    <div><span>M</span></div>
+                    <div><span>L</span></div>
+                    <div><span>XL</span></div> 
+                </div>
+                <div class="product-info">
+                    <a href="#">Details</a>
+                    <a href="#">Order</a>
+                    <a href="#">Payment</a>
+                </div>
+                <div class="small-details">
+                    <span>Composition</span>
+                    <span>Country</span>
+                </div>
+                <div class="small-details">
+                    <span>${products['coats'][dataId].composition}</span>
+                    <span>${products['coats'][dataId].country}</span>
+                </div>
+                <span>Care</span>
+                <p>${products['coats'][dataId].care}</p>
+                <div class="btn"><button>Add to Cart</button></div>
+                    
+            </div>
+        </div>
+    </div>`
 
+        modals.append(overlay);
 
+        $('.close-overlay').on('click',  function() {
+            $('.overlay').hide();
+        })
+
+    })
+
+    
 
 
 
