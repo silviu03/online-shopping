b41152ec774f9565dc658f2c6a2e267bb8ff5c5f
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-08-24 20:55:19 +0300

    work in progress

--- online-shopping.js
+++ online-shopping.js
@@ -18,7 +18,72 @@ $(function () {
         let productsObj = products['coats'][i],
             productsHTML = getPropertyHTML(productsObj);
         productsWrapper.append(productsHTML);
+        $(".products-wrapper").find(`[data-id=${productsObj.id}]`).addClass('coats-item')
     }
+    for (let i = 0; i < products['dresses'].length; i++) {
+        let productsObj = products['dresses'][i],
+            productsHTML = getPropertyHTML(productsObj);
+        productsWrapper.append(productsHTML);
+        $(".products-wrapper").find(`[data-id=${productsObj.id}]`).addClass('hidden dresses-item')
+
+    }
+    for (let i = 0; i < products['jersey'].length; i++) {
+        let productsObj = products['jersey'][i],
+            productsHTML = getPropertyHTML(productsObj);
+        productsWrapper.append(productsHTML);
+        $(".products-wrapper").find(`[data-id=${productsObj.id}]`).addClass('hidden jersey-item')
+    }
+    for (let i = 0; i < products['pants'].length; i++) {
+        let productsObj = products['pants'][i],
+            productsHTML = getPropertyHTML(productsObj);
+        productsWrapper.append(productsHTML);
+        $(".products-wrapper").find(`[data-id=${productsObj.id}]`).addClass('hidden pants-item')
+    }
+
+    const coats = $('.coats'),
+            dresses = $('.dresses'),
+            jerseys = $('.jerseys'),
+            pants = $('.pants'),
+            item = $('.item');
+
+    item.click(function(event) {
+        item.removeClass('active')
+        $(event.target).addClass('active')
+    })
+    coats.click(function() {
+        if($('.coats-item').hasClass('hidden')) {
+            $('.coats-item').removeClass('hidden')
+            $('.dresses-item').addClass('hidden')
+            $('.jersey-item').addClass('hidden')
+            $('.pants-item').addClass('hidden')
+            
+        }
+    }) 
+    dresses.click(function() {
+        if($('.dresses-item').hasClass('hidden')) {
+            $('.dresses-item').removeClass('hidden')
+            $('.coats-item').addClass('hidden')
+            $('.jersey-item').addClass('hidden')
+            $('.pants-item').addClass('hidden')
+            
+        }
+    })  
+    jerseys.click(function() {
+        if($('.jersey-item').hasClass('hidden')) {
+            $('.jersey-item').removeClass('hidden')
+            $('.dresses-item').addClass('hidden')
+            $('.coats-item').addClass('hidden')
+            $('.pants-item').addClass('hidden')
+        }
+    })  
+    pants.click(function() {
+        if($('.pants-item').hasClass('hidden')) {
+            $('.pants-item').removeClass('hidden')
+            $('.dresses-item').addClass('hidden')
+            $('.jersey-item').addClass('hidden')
+            $('.coats-item').addClass('hidden')
+        }
+    })     
 
     const productImg = $('.product-img');
 
@@ -29,7 +94,7 @@ $(function () {
 
         let productBgImg = $('.product-bg-img')
         productBgImg.css("background-image", `url('poze/${products['coats'][dataId].imgUrl}`)
-        let productCoatName = $('.product-coat-name')
+        let productCoatName = $('.product-name')
         productCoatName.text(`${products['coats'][dataId].name}`)
         let productPrice = $('.product-currency-price')
         productPrice.text(`${products['coats'][dataId].currency} ${products['coats'][dataId].price}`)
@@ -39,6 +104,72 @@ $(function () {
         productCountry.text(`${products['coats'][dataId].country}`)
         let productCare = $('.product-care')
         productCare.text(`${products['coats'][dataId].care}`)
+
+        $('.close-overlay').on('click', function () {
+            $('.overlay').addClass('hidden');
+        })
+    })
+    productImg.on('click', function (event) {
+        let dataId = event.target.dataset.id - 1;
+        const overlay = $('.overlay')
+        overlay.removeClass('hidden')
+
+        let productBgImg = $('.product-bg-img')
+        productBgImg.css("background-image", `url('poze/${products['dresses'][dataId].imgUrl}`)
+        let productCoatName = $('.product-name')
+        productCoatName.text(`${products['dresses'][dataId].name}`)
+        let productPrice = $('.product-currency-price')
+        productPrice.text(`${products['dresses'][dataId].currency} ${products['dresses'][dataId].price}`)
+        let productCompozition = $('.product-composition')
+        productCompozition.text(`${products['dresses'][dataId].composition}`)
+        let productCountry = $('.product-country')
+        productCountry.text(`${products['dresses'][dataId].country}`)
+        let productCare = $('.product-care')
+        productCare.text(`${products['dresses'][dataId].care}`)
+
+        $('.close-overlay').on('click', function () {
+            $('.overlay').addClass('hidden');
+        })
+    })
+    productImg.on('click', function (event) {
+        let dataId = event.target.dataset.id - 1;
+        const overlay = $('.overlay')
+        overlay.removeClass('hidden')
+
+        let productBgImg = $('.product-bg-img')
+        productBgImg.css("background-image", `url('poze/${products['jersey'][dataId].imgUrl}`)
+        let productCoatName = $('.product-name')
+        productCoatName.text(`${products['jersey'][dataId].name}`)
+        let productPrice = $('.product-currency-price')
+        productPrice.text(`${products['jersey'][dataId].currency} ${products['jersey'][dataId].price}`)
+        let productCompozition = $('.product-composition')
+        productCompozition.text(`${products['jersey'][dataId].composition}`)
+        let productCountry = $('.product-country')
+        productCountry.text(`${products['jersey'][dataId].country}`)
+        let productCare = $('.product-care')
+        productCare.text(`${products['jersey'][dataId].care}`)
+
+        $('.close-overlay').on('click', function () {
+            $('.overlay').addClass('hidden');
+        })
+    })
+    productImg.on('click', function (event) {
+        let dataId = event.target.dataset.id - 1;
+        const overlay = $('.overlay')
+        overlay.removeClass('hidden')
+
+        let productBgImg = $('.product-bg-img')
+        productBgImg.css("background-image", `url('poze/${products['pants'][dataId].imgUrl}`)
+        let productCoatName = $('.product-name')
+        productCoatName.text(`${products['pants'][dataId].name}`)
+        let productPrice = $('.product-currency-price')
+        productPrice.text(`${products['pants'][dataId].currency} ${products['pants'][dataId].price}`)
+        let productCompozition = $('.product-composition')
+        productCompozition.text(`${products['pants'][dataId].composition}`)
+        let productCountry = $('.product-country')
+        productCountry.text(`${products['pants'][dataId].country}`)
+        let productCare = $('.product-care')
+        productCare.text(`${products['pants'][dataId].care}`)
 
         $('.close-overlay').on('click', function () {
             $('.overlay').addClass('hidden');
--- products.js
+++ products.js
@@ -40,5 +40,132 @@ let products = {
             country: "Poland",
             care: "Follow the care label on your coat when you cleaning and treat any stains promptly by blotting."
         }
+    ],
+
+    dresses: [
+        {
+            id: 5,
+            name: "Top Dress",
+            currency: "$",
+            price: "80",
+            imgUrl: "top-dress.jpg",
+            composition: "100% cotton",
+            country: "France",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 6,
+            name: "Red Dress",
+            currency: "$",
+            price: "65",
+            imgUrl: "red-dress.jpg",
+            composition: "100% cotton",
+            country: "Spain",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 7,
+            name: "Short Dress",
+            currency: "$",
+            price: "70",
+            imgUrl: "short-dress.jpg",
+            composition: "100% cotton",
+            country: "France",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 8,
+            name: "Black Dress",
+            currency: "$",
+            price: "65",
+            imgUrl: "black-dress.jpg",
+            composition: "100% cotton",
+            country: "Italy",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        }
+    ],
+    jersey: [
+        {
+            id: 9,
+            name: "Top Jersey",
+            currency: "$",
+            price: "80",
+            imgUrl: "top-jersey.jpg",
+            composition: "100% cotton",
+            country: "France",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 10,
+            name: "Red Jersey",
+            currency: "$",
+            price: "65",
+            imgUrl: "red-jersey.png",
+            composition: "100% cotton",
+            country: "Spain",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 11,
+            name: "Blue Jersey",
+            currency: "$",
+            price: "70",
+            imgUrl: "blue-jersey.jpg",
+            composition: "100% cotton",
+            country: "France",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 12,
+            name: "Black Jersey",
+            currency: "$",
+            price: "65",
+            imgUrl: "black-jersey.png",
+            composition: "100% cotton",
+            country: "Italy",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        }
+    ],
+    pants: [
+        {
+            id: 13,
+            name: "Top Pants",
+            currency: "$",
+            price: "80",
+            imgUrl: "top-pants.jpg",
+            composition: "100% cotton",
+            country: "France",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 14,
+            name: "Red Pants",
+            currency: "$",
+            price: "65",
+            imgUrl: "red-pants.jpg",
+            composition: "100% cotton",
+            country: "Spain",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 15,
+            name: "Short Pants",
+            currency: "$",
+            price: "70",
+            imgUrl: "short-pants.jpg",
+            composition: "100% cotton",
+            country: "France",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 16,
+            name: "Black Pants",
+            currency: "$",
+            price: "65",
+            imgUrl: "black-pants.jpg",
+            composition: "100% cotton",
+            country: "Italy",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        }
     ]
 }
