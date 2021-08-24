de37415c2cd14d39267f816715bac0aa569fd368
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-08-24 18:26:15 +0300

    reworked after review

--- online-shopping.js
+++ online-shopping.js
@@ -1,8 +1,6 @@
 $(function () {
-    // sa elimini console.log inainte de livrare, e ok sa livrezi clean fara urme de debug
-    console.log('ready');
 
-    const productsWrapper = $('.produtcs-wrapper'); //atentie la typos
+    const productsWrapper = $('.products-wrapper');
     console.log(productsWrapper);
 
     getPropertyHTML = function (productsObj) {
@@ -14,77 +12,37 @@ $(function () {
                         <span>${productsObj.currency} ${productsObj.price}</span>
                     </div>
                 </div>`
-
     }
-    console.log('sec');
-
 
     for (let i = 0; i < products['coats'].length; i++) {
-        console.log('tr');
         let productsObj = products['coats'][i],
             productsHTML = getPropertyHTML(productsObj);
         productsWrapper.append(productsHTML);
-        
     }
 
     const productImg = $('.product-img');
-    const modals = $('#modals');
 
     productImg.on('click', function (event) {
-        console.log('first')
-        let dataId = event.target.dataset.id -1;
-        console.log(dataId)
-        // nu are mult sens sa creem overlay-ul de fiecare data cand dam click pe un produs, acesta poate sa existe in structura, dar ascuns, iar la click pe produs cu selectii doar sa populam dinamic continutul dorit (text, imagini, etc)
-        let overlay = `<div class="overlay" data-id=${products['coats'][dataId].id}>
-        <div class="close-overlay"><i class="fas fa-times"></i></div>
-        <div class="overlay-product-wrapper">
-            <div style="background-image: url('poze/${products['coats'][dataId].imgUrl}')"></div>
-            <div>
-                <h1>${products['coats'][dataId].name}</h1>
-                <span>${products['coats'][dataId].currency} ${products['coats'][dataId].price}</span>
-                <span>Your Size</span>
-                <div class="product-size">
-                    <div><span>S</span></div>
-                    <div><span>M</span></div>
-                    <div><span>L</span></div>
-                    <div><span>XL</span></div> 
-                </div>
-                <div class="product-info">
-                    <a href="#">Details</a>
-                    <a href="#">Order</a>
-                    <a href="#">Payment</a>
-                </div>
-                <div class="small-details">
-                    <span>Composition</span>
-                    <span>Country</span>
-                </div>
-                <div class="small-details">
-                    <span>${products['coats'][dataId].composition}</span>
-                    <span>${products['coats'][dataId].country}</span>
-                </div>
-                <span>Care</span>
-                <p>${products['coats'][dataId].care}</p>
-                <div class="btn"><button>Add to Cart</button></div>
-                    
-            </div>
-        </div>
-    </div>`
+        let dataId = event.target.dataset.id - 1;
+        const overlay = $('.overlay')
+        overlay.removeClass('hidden')
 
-        modals.append(overlay);
+        let productBgImg = $('.product-bg-img')
+        productBgImg.css("background-image", `url('poze/${products['coats'][dataId].imgUrl}`)
+        let productCoatName = $('.product-coat-name')
+        productCoatName.text(`${products['coats'][dataId].name}`)
+        let productPrice = $('.product-currency-price')
+        productPrice.text(`${products['coats'][dataId].currency} ${products['coats'][dataId].price}`)
+        let productCompozition = $('.product-composition')
+        productCompozition.text(`${products['coats'][dataId].composition}`)
+        let productCountry = $('.product-country')
+        productCountry.text(`${products['coats'][dataId].country}`)
+        let productCare = $('.product-care')
+        productCare.text(`${products['coats'][dataId].care}`)
 
-        $('.close-overlay').on('click',  function() {
-            $('.overlay').hide();
+        $('.close-overlay').on('click', function () {
+            $('.overlay').addClass('hidden');
         })
-
     })
-
-    // din nou, atentie la tot spatiul asta gol inutil aici
-
-
-
-
-
-
-
 
 })
